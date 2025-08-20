f4bb477f2a8f8621acedf20a0195ba07aa307811
M Bahrudin <muhammadbahrudin1409@gmail.com>
2025-08-20 03:01:59 +0000

    coba anda cari animasi beground yang keren dari web react bist yang coco

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,12 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
         <div className="relative flex min-h-dvh flex-col bg-transparent">
+          <div className="absolute top-0 left-0 -z-10 h-full w-full">
+            <div className="absolute top-0 left-0 h-full w-full bg-background">
+              <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-primary/10 blur-[120px] animate-blob-1"></div>
+              <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-secondary/10 blur-[120px] animate-blob-2"></div>
+            </div>
+          </div>
           <SiteHeader />
           <main className="flex-1">{children}</main>
           <SiteFooter />
