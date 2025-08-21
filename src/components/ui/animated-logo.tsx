e53fa0b9733eb1bb4a362af8dac2846d9d42f25f
M Bahrudin <muhammadbahrudin1409@gmail.com>
2025-08-21 04:09:43 +0000

    buat 2 garisnya 1 lagi warna biru nanti elemen yang warna biru nya dipin

--- src/components/ui/animated-logo.tsx
+++ src/components/ui/animated-logo.tsx
@@ -6,21 +6,24 @@ import Image from 'next/image';
 export function AnimatedLogo() {
   return (
     <div className="relative flex items-center justify-center w-full h-full aspect-square">
-      {/* Orbiting Shapes */}
+      {/* Orbiting Shapes - Pink/Primary */}
       <div className="absolute w-full h-full animate-spin-slow" style={{ animationDuration: '25s' }}>
         <div className="absolute top-[15%] left-[15%] w-10 h-10 bg-primary/50 rounded-lg"></div>
-        <div className="absolute bottom-[20%] right-[10%] w-12 h-12 bg-blue-400/50 rounded-full"></div>
-      </div>
-      <div className="absolute w-full h-full animate-spin-slow" style={{ animationDuration: '20s', animationDirection: 'reverse' }}>
-         <div className="absolute bottom-[10%] left-[20%] w-8 h-8 bg-primary/70 rounded-full"></div>
-         <div className="absolute top-[10%] right-[25%] w-6 h-6 bg-blue-400/70 rounded-md"></div>
+        <div className="absolute bottom-[10%] left-[20%] w-8 h-8 bg-primary/70 rounded-full"></div>
       </div>
 
-      {/* Rotating Dashed Circle */}
+       {/* Orbiting Shapes - Blue */}
+      <div className="absolute w-full h-full animate-spin-slow" style={{ animationDuration: '35s', animationDirection: 'reverse' }}>
+        <div className="absolute bottom-[20%] right-[10%] w-12 h-12 bg-blue-400/50 rounded-full"></div>
+        <div className="absolute top-[10%] right-[25%] w-6 h-6 bg-blue-400/70 rounded-md"></div>
+      </div>
+      
+      {/* Rotating Dashed Circles */}
       <div className="absolute w-[85%] h-[85%] border-2 border-dashed border-primary/40 rounded-full animate-spin-slow" style={{ animationDuration: '60s' }}></div>
+      <div className="absolute w-[70%] h-[70%] border-2 border-dashed border-blue-400/40 rounded-full animate-spin-slow" style={{ animationDuration: '50s', animationDirection: 'reverse' }}></div>
 
       {/* Central Logo Container */}
-      <div className="relative w-[70%] h-[70%]">
+      <div className="relative w-[65%] h-[65%]">
         <div className="absolute inset-0 bg-gradient-to-br from-pink-50 to-pink-200 dark:from-primary/10 dark:to-primary/30 rounded-full shadow-lg"></div>
         <div className="relative p-4 w-full h-full flex items-center justify-center">
             <Image
